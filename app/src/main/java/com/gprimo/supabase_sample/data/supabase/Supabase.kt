d4b170fc0827ef802243632dbade2824274d9d50
Gustavo Primo <santosprimo@gmail.com>
2023-07-10 06:56:32 -0700

    Update Supabase.kt
    Added placeholder to url and key supabase

--- app/src/main/java/com/gprimo/supabase_sample/data/supabase/Supabase.kt
+++ app/src/main/java/com/gprimo/supabase_sample/data/supabase/Supabase.kt
@@ -13,7 +13,7 @@ class Supabase {
     }
 
     companion object {
-        const val URL = "https://aguunbhxzbdqoivmnqtb.supabase.co"
-        const val KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFndXVuYmh4emJkcW9pdm1ucXRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYwOTUwOTEsImV4cCI6MjAwMTY3MTA5MX0.6SCDu38ZcMIadk2e66jPV5CAsTtXPjqiztVj_LPosFc"
+        const val URL = "SUPABASE_URL"
+        const val KEY = "SUPABASE_KEY"
     }
 }
